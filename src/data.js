fc8ca05e477f18c228490027f71f25f473d84e20
Marco Arias <marcoarccha@hotmail.com>
2024-05-10 10:17:46 -0600

    Data object for accordian.

---
+++ src/data.js
@@ -0,0 +1,21 @@
+const data = [
+    {
+        id: "1",
+        question: "Question 1?",
+        answer: "answer 1"
+    },
+
+    {
+        id: "2",
+        question: "Question 2?",
+        answer: "answer 2"
+    },
+    {
+        id: "3",
+        question: "Question 3?",
+        answer: "answer 3"
+    },
+
+];
+
+export default  data;
