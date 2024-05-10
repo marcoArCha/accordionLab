f80b89defeb936dfdbcb4d47d8ac58ecda8e8398
Marco Arias <marcoarccha@hotmail.com>
2024-05-10 10:17:00 -0600

    Accordian component added to App

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,17 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
+
 import './App.css'
+import "./components/accordian"
+import Accordian from './components/accordian'
 
 function App() {
-  const [count, setCount] = useState(0)
 
   return (
+
     <>
-      <h1>Hola</h1>
+      <Accordian />
     </>
+
+
   )
 }
 
